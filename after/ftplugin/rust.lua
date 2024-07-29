6bd3f3a1f02d2e37becac63e36fc7466b2fca4f0
Nisal Don <nisalvd@gmail.com>
2024-07-29 19:40:21 +1000

    fixed rust which-key upgrade

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -3,26 +3,27 @@ local bufnr = vim.api.nvim_get_current_buf()
 local function register_rust_keymaps(buffnr)
   local wk = require("which-key")
   local opts = { noremap = true, silent = true, buffer = buffnr, prefix = "<leader>" }
-  wk.register({
-    c = {
-      r = { "<cmd>RustLsp runnables<cr>", "rust run" },
-      l = {
-        function()
-          vim.lsp.codelens.run()
-        end,
-        "rust codelens",
-      },
-      t = {
-        "toggle things",
-        i = {
-          function()
-            require("lsp-inlayhints").toggle()
-          end,
-          "toggle inlay hints",
-        },
-      },
+  wk.add({
+    { "<leader>cr", "<cmd>RustLsp runnables<cr>", desc = "rust run", remap = false, buffer = buffnr },
+    {
+      "<leader>cl",
+      function()
+        vim.lsp.codelens.run()
+      end,
+      desc = "rust codelens",
+      remap = false,
+      buffer = buffnr,
     },
-  }, opts)
+    {
+      "<leader>cti",
+      function()
+        require("lsp-inlayhints").toggle()
+      end,
+      desc = "toggle inlay hints",
+      remap = false,
+      buffer = buffnr,
+    },
+  })
 end
 
 vim.cmd([[
