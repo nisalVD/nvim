90b9c481068ee7ee70c3c4a1a76da48bd10bab75
Nisal Don <nisalvd@gmail.com>
2024-08-05 21:27:19 +1000

    remove jira keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,18 +1,8 @@
 local keymap = vim.keymap.set
-local jira_stuff = require("config.jira-stuff")
 local utils = require("config.utils")
 
 -- rebind alternate file key to alt enter
 keymap("n", "<leader>f", ":Ex<cr>", { noremap = true })
-
--- open todo
-keymap("n", "<leader>jt", function()
-  jira_stuff.open_markdown_window("/Users/nisaldon/personal/todo.md")
-end, { noremap = true })
--- conver to jira link
-keymap("n", "<leader>jl", function()
-  jira_stuff.convert_jira_link()
-end, { noremap = true })
 
 local function toggle_qf()
   local res = vim.fn.getqflist({ winid = 1 })["winid"]
